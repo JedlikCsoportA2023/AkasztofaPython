d2ebbc84b78e56a815db31d9f1967d24d876fa15
Danka-Andras <danka.andras.balint@students.jedlik.eu>
2023-12-14 08:12:43 +0100

    Alap változók és szavak létrehozása

---
+++ functions.py
@@ -0,0 +1,8 @@
+import random
+
+szavak = ['kuglóf', 'máté', 'parízer', 'trombita', 'india', 'nigéria', 'paprika', 'libacomb', 'róka', 'banán', 'koldus', 'brokkoli', 'yasuo', 'github', 'istván', 'spagetti', 'peppino', 'pizzas']
+randomsz = [random.choice(szavak)]
+random_szo = []
+reszleges_szo = []
+jelenlegi_index = 0
+felhasznalt_betuk = []
