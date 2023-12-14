d759428c85c9d42208412905c445a684289ff8c6
Marci <banai.adrian.marton@students.jedlik.eu>
2023-12-14 08:29:56 +0100

    Betu_tipp elkezdese

--- functions.py
+++ functions.py
@@ -20,3 +20,13 @@ def reszleges_szo_lista() -> list:
     print('')
 
     return reszleges_szo
+
+def betu_tipp():
+    probalkozasok_szama = 2 * len(random_szo)
+    print(f'Hátralevő próbálkozások:{probalkozasok_szama}')
+
+
+    while reszleges_szo != random_szo and probalkozasok_szama >= 1:
+        jelenlegi_index = -1
+        tipp_betu = input('Adj meg egy betűt: ')
+        felhasznalt_betuk.append(tipp_betu)
